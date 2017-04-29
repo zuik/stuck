dd7215e5e385cb60107512fb7e52095b1db64871
sarmsaad <sarah.msaad@yahoo.com>
2017-04-29 16:35:53 -0400

    Work

--- defit/classify.py
+++ defit/classify.py
@@ -11,9 +11,21 @@ def define(word, language='en'):
     url = "{}{}/{}".format(OXFORD_API_ROOT,language, word.lower())
     r = requests.get(url, headers={'app_id': APP_ID, 'app_key': APP_KEY})
     df = r.json()
-    dff = df["results"][0]["lexicalEntries"][0]["entries"][0]["senses"][0]["definitions"][0]
-    return dff
+    arr =[]
+    length = len(df["results"][0]["lexicalEntries"])
+    for i in range(0,length):
+        arr.append(df["results"][0]["lexicalEntries"][i]["entries"][0]["senses"][0]["definitions"][0])
+    string = ""
+    for j in range(0,len(arr)):
+        string  = string + "def " + str(j+1)+ ": " + arr[j] + "\n"
+    return string
 
+#def synonym(word, language = 'en'):
 
+<<<<<<< Updated upstream
 #print(define("hypocrite"))
 
+=======
+
+print(define("break"))
+>>>>>>> Stashed changes
