c51caf2ef0f42b977bc49e1986005947f26c9d2c
Duy Nguyen <zuikng@gmail.com>
2017-04-29 19:51:39 +0000

    Define

--- defit/handler.py
+++ defit/handler.py
@@ -1,5 +1,7 @@
 from flask import Flask, request
 from twilio.rest import Client
+from twilio.twiml.messaging_response import MessagingResponse
+
 
 app = Flask(__name__)
 
@@ -8,7 +10,25 @@ TTOKEN = "5d19e13da333c40b5919746f06169ecd"
 
 tclient = Client(TSID, TTOKEN)
 
-@app.route("/sms")
+@app.route("/sms", methods=['POST'])
 def sms_handler():
-    msg = request.args.get("msg")
+    msg = request.form['Body']
+    fr_num = request.form['From']
+    if msg.split(" ")[0] == 'define':
+        words = msg.split(" ")[1:]
+        words = " ".join(words)
+        resp = MessagingResponse()
+        resp.message("You want to define: {}".format(words))
+    return str(resp)
+
+@app.route("/")
+def index():
+    return "Hello, world"
+
+if __name__ == "__main__":
+    app.run(debug=True, port=8080, host="0.0.0.0")
+
+# @app.route("/sms")
+# def sms_handler():
+#     msg = request.args.get("msg")
 
