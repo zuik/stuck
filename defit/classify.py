e8a29a973a3128153a4c528770f53359c8f7891d
sarmsaad <sarah.msaad@yahoo.com>
2017-04-29 17:25:24 -0400

    Work

--- defit/classify.py
+++ defit/classify.py
@@ -7,6 +7,7 @@ APP_KEY = 'c8faffa03a6b5c782dc4dc6580c0d1e0'
 OXFORD_API_ROOT = "https://od-api.oxforddictionaries.com:443/api/v1/entries/"
 
 
+
 def define(word, language='en'):
     url = "{}{}/{}".format(OXFORD_API_ROOT,language, word.lower())
     r = requests.get(url, headers={'app_id': APP_ID, 'app_key': APP_KEY})
@@ -20,5 +21,18 @@ def define(word, language='en'):
         string  = string + "def " + str(j+1)+ ": " + arr[j] + "\n"
     return string
 
-print(define("break"))
+def synonym(word, language='en',type="origin"):
+    url = "{}{}/{}/{}".format(OXFORD_API_ROOT, language, word.lower(),"synonyms")
+    r = requests.get(url, headers={'app_id': APP_ID, 'app_key': APP_KEY})
+    df = r.json()
+    arr=[]
+    length=len(df["results"][0]["lexicalEntries"][0]["entries"][0]["senses"][0]["synonyms"])
+    for i in range(0,length):
+        arr.append(df["results"][0]["lexicalEntries"][0]["entries"][0]["senses"][0]["synonyms"][i]["id"])
+    string = ""
+    for j in range(0,len(arr)):
+        string = string + str(j+1) + ": "+arr[j]+"\n"
+    return string
+
+print(synonym("play"))
 
