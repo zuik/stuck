2bed8c1b2a26c3cf4477865712fe6ffc010f2b0d
Duy Nguyen <zuikng@gmail.com>
2017-04-29 16:01:48 -0400

    Clean up

--- defit/classify.py
+++ defit/classify.py
@@ -2,24 +2,22 @@
 import json
 import requests
 
-app_id = '230cac46'
-app_key = 'c8faffa03a6b5c782dc4dc6580c0d1e0'
+APP_ID = '230cac46'
+APP_KEY = 'c8faffa03a6b5c782dc4dc6580c0d1e0'
+
+OXFORD_API_ROOT = "https://od-api.oxforddictionaries.com:443/api/v1/entries/"
 
 language = 'en'
 
-##requests = urlopen("http://www.dictionaryapi.com/api/v1/references/collegiate/xml/test?key=883faa22-6561-4b2c-8525-64b8979f2953")
-##requests = urlopen("https://od-api.oxforddictionaries.com/api/v1")
-##response= requests.read()
 
-
-##XML_understand = 
-
-def printDEF(word_id):
-    url = 'https://od-api.oxforddictionaries.com:443/api/v1/entries/' + language + '/' + word_id.lower()
-    r = requests.get(url, headers = {'app_id': app_id, 'app_key': app_key})
+def define(word):
+    url = "{}{}/".format(OXFORD_API_ROOT, language, word.lower())
+    r = requests.get(url, headers={'app_id': APP_ID, 'app_key': APP_KEY})
+    defition = ""
     print("code {}\n".format(r.status_code))
     print("text \n" + r.text)
     print("json \n" + json.dumps(r.json()))
+    return definition
 
 
 printDEF("gay")
