470124743fb4e9f5825889c70ab70e693b0a089f
Duy Nguyen <zuikng@gmail.com>
2017-04-29 15:45:42 -0400

    Update handler to response to Msg

--- defit/handler.py
+++ defit/handler.py
@@ -1,5 +1,7 @@
 from flask import Flask, request
 from twilio.rest import Client
+from twilio.twiml.messaging_response import MessagingResponse
+
 
 app = Flask(__name__)
 
@@ -8,7 +10,21 @@ TTOKEN = "5d19e13da333c40b5919746f06169ecd"
 
 tclient = Client(TSID, TTOKEN)
 
-@app.route("/sms")
+@app.route("/sms", methods=['POST'])
 def sms_handler():
-    msg = request.args.get("msg")
+    print(request.form['Body'])
+    resp = MessagingResponse()
+    resp.message("Blah blah")
+    return str(resp)
+
+@app.route("/")
+def index():
+    return "Hello, world"
+
+if __name__ == "__main__":
+    app.run(debug=True, port=8080, host="0.0.0.0")
+
+# @app.route("/sms")
+# def sms_handler():
+#     msg = request.args.get("msg")
 
