82097c6f53fe0930133a1bebfafaaffa1871af9b
Duy Nguyen <zuikng@gmail.com>
2017-04-29 16:14:40 -0400

    Cleanning

--- defit/classify.py
+++ defit/classify.py
@@ -7,14 +7,13 @@ APP_KEY = 'c8faffa03a6b5c782dc4dc6580c0d1e0'
 
 OXFORD_API_ROOT = "https://od-api.oxforddictionaries.com:443/api/v1/entries/"
 
-language = 'en'
 
-
-def define(word):
+def define(word, language='en'):
     url = "{}{}/{}".format(OXFORD_API_ROOT,language, word.lower())
     r = requests.get(url, headers={'app_id': APP_ID, 'app_key': APP_KEY})
-    d= r.json()
-    return d["results"][0]["lexicalEntries"][0]["entries"][0]["senses"][0]["definitions"][0]
+    df = r.json()
+    dff = df["results"][0]["lexicalEntries"][0]["entries"][0]["senses"][0]["definitions"][0]
+    return dff
 
 
 print(define("hypocrite"))
