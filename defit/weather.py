80b7002cc8dc4a582cc082370c652cb2399f4741
sarmsaad <sarah.msaad@yahoo.com>
2017-04-30 01:58:59 -0400

    Work

--- defit/weather.py
+++ defit/weather.py
@@ -1,6 +1,5 @@
 import json
 import requests
-import urllib
 
 API_KEY = "53c68ea7dcab4200e1b5edc1f9f1fe71"
 
@@ -16,4 +15,19 @@ def weather(location):
 def round_up(x, place):
     return round(x + 5 * 10**(-1 * (place + 1)), place)
 
+def set_rise(location, what):
+    url = "{}?q={}&APPID={}".format(PRIMARY, location, API_KEY)
+    response = requests.get(url)
+    df = response.json()
+    long=df["coord"]["lon"]
+    lati=df["coord"]["lat"]
+    url_sun = "https://api.sunrise-sunset.org/json?lat={}&lng={}".format(str(lati),str(long))
+    response_sun = requests.get(url_sun)
+    dff = response_sun.json()
+    if(what == "sunset"):
+        return dff["results"]["sunset"]
+    else:
+        return dff["results"]["sunrise"]
+
 weather("London")
+print(set_rise("Boston","sunset"))
